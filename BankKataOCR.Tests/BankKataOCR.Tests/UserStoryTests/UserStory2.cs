d9c34bf8b1c7c388ec5bd0927405d5b9392babcd
Daniel Barnett <b4barnett@googlemail.com>
2022-01-26 23:15:46 +0000

    Figured out story 2 (please dont judge i thought it was some mad substition thing, not that numbers above dx was just an example

--- BankKataOCR.Tests/BankKataOCR.Tests/UserStoryTests/UserStory2.cs
+++ BankKataOCR.Tests/BankKataOCR.Tests/UserStoryTests/UserStory2.cs
@@ -1,19 +1,36 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 
 namespace BankOcrKata
 {
     [TestFixture]
     public class UserStory2
     {
-        [TestCase("711111111", true)]
-        [TestCase("123456789", true)]
-        [TestCase("490867715", true)]
-        [TestCase("888888888", false)]
-        [TestCase("490067715", false)]
-        [TestCase("012345678", false)]
-        public void Tests(string accountNumber, bool isValid)
+        [TestCase( "711111111", true )]
+        [TestCase( "123456789", true )]
+        [TestCase( "490867715", true )]
+        [TestCase( "888888888", false )]
+        [TestCase( "490067715", false )]
+        [TestCase( "012345678", false )]
+        public void Tests( string accountNumber, bool isValid )
         {
-            Assert.Fail();
+            DoCheckSome( accountNumber.ToCharArray(), isValid );
+        }
+
+        private static void DoCheckSome( char[] charArray, bool result )
+        {
+            int total = 0;
+            int counter = 9;
+            foreach ( var c in charArray )
+            {
+                int value = int.Parse( c.ToString() );
+                total += ( value * counter );
+                counter = counter - 1;
+            }
+
+            int modulus = total % 11;
+
+            modulus.Equals( 0 ).Should().Be( result );
         }
     }
 }
