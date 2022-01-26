3b969280d36bcab5eaf8c5c1ed6f96c5fce6a32a
Daniel Barnett <b4barnett@googlemail.com>
2022-01-26 20:47:14 +0000

    Basic project setup

---
+++ BankKataOCR.Tests/BankKataOCR.Tests/UserStoryTests/UserStory1.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System.IO;
+using TechTest.Services;
+
+namespace BankOcrKata
+{
+
+    //FYI I think the Kata is wrong, the description is that the fourth line is blank,
+    ////but the example data it gives and is used as part of these tests use the first line as blank
+    [TestFixture]
+    public class UserStory1
+    {
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _
+| || || || || || || || || |
+|_||_||_||_||_||_||_||_||_|", "000000000")]
+        [TestCase(@"                         
+                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |", "111111111")]
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+|_ |_ |_ |_ |_ |_ |_ |_ |_ ", "222222222")]
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _
+ _| _| _| _| _| _| _| _| _|
+ _| _| _| _| _| _| _| _| _|", "333333333")]
+        [TestCase(@"
+                           
+|_||_||_||_||_||_||_||_||_|
+  |  |  |  |  |  |  |  |  |", "444444444")]
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+ _| _| _| _| _| _| _| _| _|", "555555555")]
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+|_||_||_||_||_||_||_||_||_|", "666666666")]
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |", "777777777")]
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+|_||_||_||_||_||_||_||_||_|", "888888888")]
+        [TestCase(@"
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+ _| _| _| _| _| _| _| _| _|", "999999999")]
+        [TestCase(@"
+    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|", "123456789")]
+        public void Tests(string input, string expectedResult)
+        {
+            using ( var stringReader = new StringReader( input ) )
+            {
+                OCRReader.ReadSingleOCRLine( stringReader );
+            }
+
+            int i = 0;
+        }
+    }
+}
